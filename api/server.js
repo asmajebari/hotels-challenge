c6a06193e3f25b1dd43788b208fa8dfa4e564953
Asma Jebari <asma.jebari@insat.ucar.tn>
2022-07-16 03:45:56 +0100

    Add middleware for requests info

--- api/server.js
+++ api/server.js
@@ -6,6 +6,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => { 
+    const start = Date.now();
+    next();
+    const delta = Date.now() - start;
+    console.log(`${req.method} ${req.baseUrl}${req.url} ${res.statusCode} ${delta}ms`);
+});
+
 app.get('/', (req, res) => {
     res.send("helloo! Go to http://localhost:3000/hotels or search for hotels using stars, for example: http://localhost:3000/hotels?stars=3");
 });
